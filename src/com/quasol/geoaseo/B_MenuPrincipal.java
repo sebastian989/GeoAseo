ef238499ca576be0dbbc422e0cff0fefb5c1d5e1
perez <perez@perez-PC>
2014-07-28 16:43:29 -0500

    principal bar of principal menu

--- src/com/quasol/geoaseo/B_MenuPrincipal.java
+++ src/com/quasol/geoaseo/B_MenuPrincipal.java
@@ -1,15 +1,28 @@
 package com.quasol.geoaseo;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 public class B_MenuPrincipal extends Activity {
+	
+	private SharedPreferences sharedpreferences;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.b__menu_principal);
+		this.sharedpreferences = getSharedPreferences("MyPreferences",Context.MODE_PRIVATE);
+	}
+	
+	public void logOut(View v){
+		Editor editor = sharedpreferences.edit();
+		editor.clear();
+		editor.commit();
+		finish();
 	}
 }
