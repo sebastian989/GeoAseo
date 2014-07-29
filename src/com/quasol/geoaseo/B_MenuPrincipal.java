d4bbaa65eef44a569e5927a55c9dcfbc4916ec2f
perez <perez@perez-PC>
2014-07-29 12:58:32 -0500

    Commit for solve the conflinct with strings

--- src/com/quasol/geoaseo/B_MenuPrincipal.java
+++ src/com/quasol/geoaseo/B_MenuPrincipal.java
@@ -1,4 +1,5 @@
 package com.quasol.geoaseo;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -12,7 +13,7 @@ import android.view.View;
 import android.widget.DigitalClock;
 
 public class B_MenuPrincipal extends Activity {
-	
+
 	private SharedPreferences sharedpreferences;
 	private DigitalClock dc;
 
@@ -20,45 +21,58 @@ public class B_MenuPrincipal extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.b__menu_principal);
-		this.sharedpreferences = getSharedPreferences("MyPreferences",Context.MODE_PRIVATE);
-		this.dc = (DigitalClock)findViewById(R.id.digitalClock1);
-		
+		this.sharedpreferences = getSharedPreferences("MyPreferences",
+				Context.MODE_PRIVATE);
 	}
-	
-	public void logOut(View v){
+
+	/**
+	 * 
+	 * @param v
+	 */
+	public void logOut(View v) {
 		AlertDialog.Builder adb = new AlertDialog.Builder(this);
-        adb.setTitle(getResources().getString(R.string.logout_confirm));
-        adb.setPositiveButton(getResources().getString(R.string.logout_confirm_button_1),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog,
-                            int which) {
-                    	Editor editor = sharedpreferences.edit();
-                		editor.clear();
-                		editor.commit();
-                		finish();
-                    }
-                });
-        adb.setNegativeButton(getResources().getString(R.string.logout_confirm_button_2),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog,
-                            int which) {
-                        // TODO Auto-generated method stub
-                         dialog.dismiss();
-                    }
-                });
-        adb.show();
+		adb.setTitle(getResources().getString(R.string.logout_confirm));
+		adb.setPositiveButton(
+				getResources().getString(R.string.logout_confirm_button_1),
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						Editor editor = sharedpreferences.edit();
+						editor.clear();
+						editor.commit();
+						finish();
+					}
+				});
+		adb.setNegativeButton(
+				getResources().getString(R.string.logout_confirm_button_2),
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						dialog.dismiss();
+					}
+				});
+		adb.show();
 	}
-	
-	public void setOperators(View v){
+
+	public void setOperators(View v) {
 		Intent intent = new Intent(this, C_GrupoTrabajo.class);
 		startActivity(intent);
 	}
-	
+
+	public void startDay(View v) {
+//		if ((sharedpreferences.contains("TRUCK_INFO") && sharedpreferences.contains("TOKEN"))) {
+//			Intent intent = new Intent(this, E_MenuCiclo.class);
+//			startActivity(intent);
+//			} else {
+				Intent intent = new Intent(this, D_RegistrarDatosVehiculo.class);
+				startActivity(intent);
+//		}
+	}
+
 	@Override
-	 public boolean onKeyDown(int keyCode, KeyEvent event)  {
-		 moveTaskToBack(true);
-		 return super.onKeyDown(keyCode, event);
-	 }
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		moveTaskToBack(true);
+		return super.onKeyDown(keyCode, event);
+	}
 }
