72d4039a5e800a8eaa5f5914d14e46304d786076
perez <perez@perez-PC>
2014-07-29 21:55:16 -0500

    Data_truck info in preferences

--- src/com/quasol/geoaseo/B_MenuPrincipal.java
+++ src/com/quasol/geoaseo/B_MenuPrincipal.java
@@ -1,4 +1,6 @@
 package com.quasol.geoaseo;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -11,18 +13,22 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.DigitalClock;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class B_MenuPrincipal extends Activity {
 
 	private SharedPreferences sharedpreferences;
 	private DigitalClock dc;
+	private ImageButton team_of_work,start_day,create_route,close_route;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.b__menu_principal);
-		this.sharedpreferences = getSharedPreferences("MyPreferences",
-				Context.MODE_PRIVATE);
+		this.sharedpreferences = getSharedPreferences("MyPreferences",Context.MODE_PRIVATE);
+		initialize_visual_controls();
+		this.dc=(DigitalClock)findViewById(R.id.clock1);	
 	}
 
 	/**
@@ -61,18 +67,35 @@ public class B_MenuPrincipal extends Activity {
 	}
 
 	public void startDay(View v) {
-//		if ((sharedpreferences.contains("TRUCK_INFO") && sharedpreferences.contains("TOKEN"))) {
-//			Intent intent = new Intent(this, E_MenuCiclo.class);
-//			startActivity(intent);
-//			} else {
+		int newOrometer;
+			try {
+				JSONArray truckInformation = new JSONArray((String)sharedpreferences.getString("TRUCK_INFO",""));
+				JSONObject truckObject = truckInformation.getJSONObject(0);
+				newOrometer = truckObject.getInt("nuevo_horometro");
+			} catch (Exception e) {
+				newOrometer=0;
+			}
+			if(newOrometer!=0){
+				//Interfaz para continuar con el siclo
+				Intent intent = new Intent(this, E_MenuCiclo.class);
+				startActivity(intent);
+			}
+			else{
 				Intent intent = new Intent(this, D_RegistrarDatosVehiculo.class);
 				startActivity(intent);
-//		}
+			}
 	}
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		moveTaskToBack(true);
 		return super.onKeyDown(keyCode, event);
+	}
+	
+	public void initialize_visual_controls(){
+		this.team_of_work=(ImageButton)findViewById(R.id.btn_team_of_work);
+		this.start_day=(ImageButton)findViewById(R.id.btn_start_day);
+		this.create_route=(ImageButton)findViewById(R.id.btn_create_route);
+		this.close_route=(ImageButton)findViewById(R.id.btn_close_route);
 	}
 }
--- src/com/quasol/geoaseo/D_RegistrarDatosVehiculo.java
+++ src/com/quasol/geoaseo/D_RegistrarDatosVehiculo.java
@@ -1,15 +1,68 @@
 package com.quasol.geoaseo;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.Intent;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class D_RegistrarDatosVehiculo extends Activity {
 
+	private SharedPreferences sharedpreferences;
+	private EditText hourmeter,odometer;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.d__registrar_datos_vehiculo);
+		this.hourmeter=(EditText)findViewById(R.id.editText_hourmeter);
+		this.odometer=(EditText)findViewById(R.id.editText_odometer);
+		this.sharedpreferences = getSharedPreferences("MyPreferences",Context.MODE_PRIVATE);
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+	}
+	
+	public void Save_truck_information(View v){
+		
+		if(this.hourmeter.getText().equals("")||this.odometer.getText().equals("") ){
+			Toast.makeText(this, "Complete los campos", Toast.LENGTH_LONG).show();
+		}
+		else{
+			try {
+				JSONArray truckInformation = new JSONArray((String)sharedpreferences.getString("TRUCK_INFO",""));
+				if((int)truckInformation.getJSONObject(0).getInt("horometro")<=Integer.parseInt(this.hourmeter.getText().toString())){
+					if((int)truckInformation.getJSONObject(0).getInt("odometro")<=Integer.parseInt(this.odometer.getText().toString())){
+						
+						truckInformation.getJSONObject(0).put("nuevo_horometro",Integer.parseInt(this.hourmeter.getText().toString()));
+						truckInformation.getJSONObject(0).put("nuevo_odometro",Integer.parseInt(this.odometer.getText().toString()));
+						SharedPreferences.Editor editor = sharedpreferences.edit();
+						editor.putString("TRUCK_INFO", truckInformation.toString());
+						editor.commit();
+						Intent intent = new Intent(this, E_MenuCiclo.class);
+						startActivity(intent);
+						finish();
+						
+					}else{
+						Toast.makeText(this, "El odometro debe ser mayor o igual a "+truckInformation.getJSONObject(0).getInt("odometro"), Toast.LENGTH_LONG).show();
+					}
+				}
+				else{
+						Toast.makeText(this, "El horometro debe ser mayor o igual a "+truckInformation.getJSONObject(0).getInt("horometro"), Toast.LENGTH_LONG).show();
+				}
+				
+			} catch (Exception e) {
+				Toast.makeText(this, "Ocurrio un problema al guardar los datos intentelo de nuevo", Toast.LENGTH_LONG).show();				
+			}
+			
+			
+		}
+		
+		
 	}
 }
