0ae19e534cfa2e362042b31627a34715fb9fde91
perez <perez@perez-PC>
2014-07-31 19:33:26 -0500

    logic Implementation of exit base button

--- src/com/quasol/geoaseo/E_MenuCiclo.java
+++ src/com/quasol/geoaseo/E_MenuCiclo.java
@@ -6,28 +6,37 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.widget.Toast;
+import android.widget.ImageButton;
 
 public class E_MenuCiclo extends Activity {
 
 	private SharedPreferences sharedpreferences;
+	private AlertDialog.Builder adb;
+	private ImageButton btn_base_exit,btn_start_collection,btn_compactation,btn_collection_finish,btn_arrive_final_disposition,btn_come_back_to_base,btn_special_service,btn_inoperability;
+	private Drawable d_base_exit,d_base_exit_two,d_start_collection,d_start_collection_two,d_compactation,d_compactation_two,d_collection_finish,d_collection_finish_two,d_arrive_final_disposition,d_arrive_final_disposition_two,d_come_back_to_base,d_come_back_to_base_two,d_special_service,d_special_service_two,d_inoperability,d_inoperability_two;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.e__menu_ciclo);
 		this.sharedpreferences = getSharedPreferences("MyPreferences",Context.MODE_PRIVATE);
+		this.initialize_components();
+		int current_state = sharedpreferences.getInt("CURRENT_STATE", 0);
+		if(current_state != 0){
+			if(current_state==1){
+				buttons_exit_base();
+			}
+		}
 	}
 	
 	@Override
 	protected void onResume() {
-		AlertDialog.Builder adb = new AlertDialog.Builder(this);
-		adb.setTitle("DEBE AGREGAR UN GRUPO DE OPERARIOS");
-		
-		adb.setPositiveButton(
+		this.adb.setTitle("DEBE AGREGAR UN GRUPO DE OPERARIOS");
+		this.adb.setPositiveButton(
 				"OK",
 				new DialogInterface.OnClickListener() {
 					@Override
@@ -38,11 +47,9 @@ public class E_MenuCiclo extends Activity {
 					}
 				});
 		String selectedOperators = sharedpreferences.getString("SELECTED_OPERATORS", null);
-		if(selectedOperators == null){
-			adb.show();
-//			Toast.makeText(this, "Debe agregar un grupo de trabajo", Toast.LENGTH_LONG).show();	
+		if(selectedOperators == null || selectedOperators.length()<=2){
+			this.adb.show();
 		}
-		
 		super.onResume();
 	}
 	
@@ -68,8 +75,116 @@ public class E_MenuCiclo extends Activity {
 	}
 	 
 	public void exit_base(View v){
+		this.adb.setTitle("ESTA SEGURO QUE VA A SALIR DE LA BASE ");
+		this.adb.setPositiveButton(
+				"SI",
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						buttons_exit_base();
+					}
+				});
+		this.adb.setNegativeButton(
+				"NO", 
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				});
+		this.adb.show();
+	}
+	
+	
+	public void buttons_exit_base(){
 		
+		SharedPreferences.Editor editor = sharedpreferences.edit();
+		int current_state = sharedpreferences.getInt("CURRENT_STATE", 0);
+		if(current_state==0){
+			editor.putInt("CURRENT_STATE", 1);
+			editor.commit();
+		}
+		this.btn_base_exit.setImageDrawable(this.d_base_exit_two);
+		this.btn_base_exit.setEnabled(false);
 		
+		this.btn_start_collection.setImageDrawable(this.d_start_collection);
+		this.btn_start_collection.setEnabled(true);
+		
+		this.btn_inoperability.setImageDrawable(this.d_inoperability);
+		this.btn_inoperability.setEnabled(true);
+		
+		this.btn_special_service.setImageDrawable(this.d_special_service);
+		this.btn_special_service.setEnabled(true);
+	}
+	
+	
+	/**
+	 * 
+	 * @param v
+	 */
+	public void logOut(View v) {
+		AlertDialog.Builder adb = new AlertDialog.Builder(this);
+		adb.setTitle(getResources().getString(R.string.logout_confirm));
+		adb.setPositiveButton(
+				getResources().getString(R.string.confirm_button_1),
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						Editor editor = sharedpreferences.edit();
+						editor.clear();
+						editor.commit();
+						finish();
+					}
+				});
+		adb.setNegativeButton(
+				getResources().getString(R.string.confirm_button_2),
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						dialog.dismiss();
+					}
+				});
+		adb.show();
+	}
+	
+public void initialize_components(){
+		
+		//Initialize controls
+		this.adb= new AlertDialog.Builder(this);
+		this.btn_base_exit=(ImageButton)findViewById(R.id.btn_base_exit);
+		this.btn_start_collection=(ImageButton)findViewById(R.id.btn_start_collection);
+		this.btn_start_collection.setEnabled(false);
+		this.btn_compactation=(ImageButton)findViewById(R.id.btn_compactation);
+		this.btn_compactation.setEnabled(false);
+		this.btn_collection_finish=(ImageButton)findViewById(R.id.btn_collection_finish);
+		this.btn_collection_finish.setEnabled(false);
+		this.btn_arrive_final_disposition=(ImageButton)findViewById(R.id.btn_arrive_final_disposition);
+		this.btn_arrive_final_disposition.setEnabled(false);
+		this.btn_come_back_to_base=(ImageButton)findViewById(R.id.btn_come_back_to_base);
+		this.btn_come_back_to_base.setEnabled(false);
+		this.btn_special_service=(ImageButton)findViewById(R.id.btn_special_service);
+		this.btn_special_service.setEnabled(false);
+		this.btn_inoperability=(ImageButton)findViewById(R.id.btn_inoperability);
+		this.btn_inoperability.setEnabled(false);
+		
+		//Inizialize drawable objects
+		this.d_base_exit = this.getResources().getDrawable(R.drawable.btn_base_exit);
+		this.d_base_exit_two = this.getResources().getDrawable(R.drawable.btn_base_exit_two);
+		this.d_start_collection = this.getResources().getDrawable(R.drawable.btn_start_collection);
+		this.d_start_collection_two = this.getResources().getDrawable(R.drawable.btn_start_collection_two);
+		this.d_compactation = this.getResources().getDrawable(R.drawable.btn_compaction);
+		this.d_compactation_two = this.getResources().getDrawable(R.drawable.btn_compaction_two);
+		this.d_collection_finish = this.getResources().getDrawable(R.drawable.btn_collection_finish);
+		this.d_collection_finish_two = this.getResources().getDrawable(R.drawable.btn_collection_finish_two);
+		this.d_arrive_final_disposition = this.getResources().getDrawable(R.drawable.btn_arrive_final_disposition);
+		this.d_arrive_final_disposition_two = this.getResources().getDrawable(R.drawable.btn_arrive_final_disposition_two);
+		this.d_come_back_to_base = this.getResources().getDrawable(R.drawable.btn_come_back_to_base);
+		this.d_come_back_to_base_two = this.getResources().getDrawable(R.drawable.btn_come_back_to_base_two);
+		this.d_special_service = this.getResources().getDrawable(R.drawable.btn_special_service);
+		this.d_special_service_two = this.getResources().getDrawable(R.drawable.btn_special_service_two);
+		this.d_inoperability = this.getResources().getDrawable(R.drawable.btn_inoperability);
+		this.d_inoperability_two = this.getResources().getDrawable(R.drawable.btn_inoperability_two);
 		
 	}
 	
